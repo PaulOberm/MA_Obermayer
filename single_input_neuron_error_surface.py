c69af4c135298e1446ffdbe84d1dcce8a076f70e
Paul Obermayer <paul.obermayer92@gmail.com>
2018-05-01 18:41:50 +0200

    Add sigmoid activation function visualization

--- single_input_neuron_error_surface.py
+++ single_input_neuron_error_surface.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[230]:
+# In[251]:
 
 
 '''
@@ -18,11 +18,30 @@ from matplotlib import cm
 from mpl_toolkits.mplot3d import axes3d
 from mpl_toolkits import mplot3d
 
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+
 from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('text', usetex=True)
 
 #get_ipython().magic('matplotlib inline')
+
+
+# In[252]:
+
+
+class Arrow3D(FancyArrowPatch):
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        FancyArrowPatch.draw(self, renderer)
 
 
 # In[34]:
@@ -94,7 +113,7 @@ for b_idx, b in enumerate(bias):
 print('Error matrix: {}'.format(error.shape))
 
 
-# In[247]:
+# In[294]:
 
 
 #Figure
@@ -102,7 +121,6 @@ fig = plt.figure()
 fig.set_size_inches(18.5, 10.5)
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
-plt.title(r"Single input neuron error surface", fontsize=20, color='black')
 plt.grid(True)
 
 #Plot
@@ -118,7 +136,26 @@ ax.set_ylabel(r'Weight $w$', fontsize=20)
 ax.set_zlabel(r'Error $E$', fontsize=20)
 #Rotation
 ax.view_init(60, 20)
+plt.title(r"Single input neuron error surface", fontsize=20, color='black')
 
+#Drawing arrows
+a = Arrow3D([X[0,2], X[3,0]], [Y[0,2], Y[3,0]], [error[0,2], error[3,0]], mutation_scale=20, arrowstyle='-|>', color='g')
+ax.add_artist(a)
+a = Arrow3D([X[3,0], X[2,6]], [Y[3,0], Y[2,6]], [error[3,0], error[2,6]], mutation_scale=20, arrowstyle='-|>', color='g')
+ax.add_artist(a)
+a = Arrow3D([X[2,6], X[5,0]], [Y[2,6], Y[5,0]], [error[2,6], error[5,0]], mutation_scale=20, arrowstyle='-|>', color='g')
+ax.add_artist(a)
+a = Arrow3D([X[5,0], X[3,8]], [Y[5,0], Y[3,8]], [error[5,0], error[3,8]], mutation_scale=20, arrowstyle='-|>', color='g')
+ax.add_artist(a)
+a = Arrow3D([X[3,8], X[7,2]], [Y[3,8], Y[7,2]], [error[3,8], error[7,2]], mutation_scale=20, arrowstyle='-|>', color='g')
+ax.add_artist(a)
+a = Arrow3D([X[7,2], X[5,12]], [Y[7,2], Y[5,12]], [error[7,2], error[5,12]], mutation_scale=20, arrowstyle='-|>', color='g')
+ax.add_artist(a)
+a = Arrow3D([X[5,12], X[10,6]], [Y[5,12], Y[10,6]], [error[5,12], error[10,6]], mutation_scale=20, arrowstyle='-|>', color='g')
+ax.add_artist(a)
+
+a = Arrow3D([X[35,0], X[5,39]], [Y[35,0], Y[5,39]], [error[35,0], error[5,39]], mutation_scale=20, color='r')
+ax.add_artist(a)
 
 #Plot
 plt.show()
